42b71a044d50dc31b284fd494a355862e7c075f2
Erin Zhang <erin-z@users.noreply.github.com>
2017-09-11 00:34:35 +1000

    Create 206_Customizing your spreadsheet import.py

---
+++ 206_Customizing your spreadsheet import.py
@@ -0,0 +1,13 @@
+'''
+Here, you'll parse your spreadsheets and use additional arguments to skip rows, rename columns and select only particular columns.
+
+The spreadsheet 'battledeath.xlsx' is already loaded as xl.
+
+As before, you'll use the method parse(). This time, however, you'll add the additional arguments skiprows, names and parse_cols. These skip rows, name the columns and designate which columns to parse, respectively. All these arguments can be assigned to lists containing the specific row numbers, strings and column numbers, as appropriate.
+Instructions
+
+    Parse the first sheet by index. In doing so, skip the first row of data and name the columns 'Country' and 'AAM due to War (2002)' using the argument names. The values passed to skiprows and names all need to be of type list.
+    Parse the second sheet by index. In doing so, parse only the first column with the parse_cols parameter, skip the first row and rename the column 'Country'. The argument passed to parse_cols also needs to be of type list.
+
+'''
+
