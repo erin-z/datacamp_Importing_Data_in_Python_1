5c74edfe17cd9514a80eaab0770da0c43d4a4367
Erin Zhang <erin-z@users.noreply.github.com>
2017-09-10 15:25:19 +1000

    Create 113_114_Working with mixed datatypes.py

---
+++ 113_114_Working with mixed datatypes.py
@@ -0,0 +1,33 @@
+#113
+'''
+Much of the time you will need to import datasets which have different datatypes in different columns; one column may contain strings and another floats, for example. The function np.loadtxt() will freak at this. There is another function, np.genfromtxt(), which can handle such structures. If we pass dtype=None to it, it will figure out what types each column should be.
+
+Import 'titanic.csv' using the function np.genfromtxt() as follows:
+
+data = np.genfromtxt('titanic.csv', delimiter=',', names=True, dtype=None)
+
+Here, the first argument is the filename, the second specifies the delimiter , and the third argument names tells us there is a header. Because the data are of different types, data is an object called a structured array. Because numpy arrays have to contain elements that are all the same type, the structured array solves this by being a 1D array, where each element of the array is a row of the flat file imported. You can test this by checking out the array's shape in the shell by executing np.shape(data).
+
+Acccessing rows and columns of structured arrays is super-intuitive: to get the ith row, merely execute data[i] and to get the column with name 'Fare', execute data['Fare'].
+
+Print the entire column with name Survived to the shell. What are the last 4 values of this column?
+'''
+
+#114
+'''
+You have just used np.genfromtxt() to import data containing mixed datatypes. There is also another function np.recfromcsv() that behaves similarly to np.genfromtxt(), except that its default dtype is None. In this exercise, you'll practice using this to achieve the same result.
+Instructions
+
+    Import titanic.csv using the function np.recfromcsv() and assign it to the variable, d. You'll only need to pass file to it because it has the defaults delimiter=',' and names=True in addition to dtype=None!
+    Run the remaining code to print the first three entries of the resulting array d.
+
+'''
+
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Import file using np.recfromcsv: d
+d = np.recfromcsv(file)
+
+# Print out first three entries of d
+print(d[:3])
